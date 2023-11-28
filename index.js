82d8e1c9158dba42312d6c6269bc6fa545e352d8
root <danugurikavya2@gmail.com>
2023-11-28 07:05:14 +0000

    Congrats Card Solution

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div class="bg-container">
+    <h1>Congratulations</h1>
+    <div class="container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h2>Kiran V</h2>
+      <p>Vishnu Institute Of Computer Education and Technology Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+
+  // Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
